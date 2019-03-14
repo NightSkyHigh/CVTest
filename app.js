1283c4fcd389c19cbd202b824d72f0b98732a504
Sanay <sanay1910@gmail.com>
2019-03-14 12:01:07 +0100

    added hbs

--- app.js
+++ app.js
@@ -2,16 +2,17 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const fs = require('fs');
+const data = require('./info.json');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.render('index', {data : data});
 });
 
 app.get('/data', (req, res) => {
-    fs.readFile('./info.json', (err, json) => {
-        let obj = JSON.parse(json);
-        res.json(obj);
-    })
+    res.json(data);
 });
 
 app.listen(process.env.PORT || 8080)
